c12995e1cb2a54d6b537f5cc2d12d470ce912fae
Mason Chang <masonchang1991@gmail.com>
2017-09-11 15:58:09 +0800

    merge develop and create LoginViewController

--- LoginViewController.m
+++ LoginViewController.m
@@ -17,21 +17,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    
+    
+    
+    [_loginButton addTarget:self action:@selector(login) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)logIn {
+    
+    
+    FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+    
+    login.loginBehavior = FBSDKLoginBehaviorBrowser;
+    [login logInWithRead:@[@"public_profile",@"email"] from
+    
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
